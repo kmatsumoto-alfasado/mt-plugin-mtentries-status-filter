66db1bb83e09ba2bb9716d06d412c97e584e2dfa
Koji Matsumoto <matsumoto@alfasado.jp>
2026-03-27 11:14:00 +0900

    support-mtpages
    ・プラグインの説明文の更新。

--- plugins/MTEntriesStatusFilter/lib/MTEntriesStatusFilter/L10N/ja.pm
+++ plugins/MTEntriesStatusFilter/lib/MTEntriesStatusFilter/L10N/ja.pm
@@ -4,7 +4,7 @@ use base 'MTEntriesStatusFilter::L10N';
 use vars qw( %Lexicon );
 
 our %Lexicon = (
-    'Add status_filter modifier to MTEntries.' => 'MTEntries ブロックタグに status_filter モディファイアを追加します。',
+    'Add status_filter modifier to MTEntries/MTpages.' => 'MTEntries/MTpages ブロックタグに status_filter モディファイアを追加します。',
  );
 
 1;
